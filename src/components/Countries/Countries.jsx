644db21f7f1990ed4ecf3ab36131d43462372759
Mushfiqur0R <musfikur.dev@gmail.com>
2024-03-13 20:45:31 +0600

    Updating State In Arrays And Pass Parameter

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -4,6 +4,15 @@ import Country from "../Country/Country";
 import './Countries.css'
 const Countries = () => {
     const [countries, setCountries] = useState([]);
+    const [visitedCountries, setVisitedCountries] = useState([]);
+
+
+    const handleVisitedCountry = country =>{
+        console.log('add this to your visited country');
+        const newVisitedCountries = [...visitedCountries, country];
+        setVisitedCountries(newVisitedCountries);
+        console.log(country);
+    }
 
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
@@ -13,10 +22,20 @@ const Countries = () => {
     return (
         <div>
             <h3>Countries: {countries.length}</h3>
+            <div>
+                <h5>Visited Countries {visitedCountries.length}</h5>
+                <ul>
+                    {
+                        visitedCountries.map(country => <li key={country.cca3}>{country.name.common}</li>)
+                    }
+                </ul>
+            </div>
             <div className="country-container">
-                {
-                    countries.map(country => <Country key={country.cca3} country={country}></Country>)
-                }
+            {
+                countries.map(country => <Country key={country.cca3} 
+                handleVisitedCountry = {handleVisitedCountry}
+                country={country}></Country>)
+            }
             </div>
         </div>
     );
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Country.css'
-const Country = ({country}) => {
+const Country = ({country, handleVisitedCountry}) => {
     const {name, flags, population, area, cca3} = country;
 
     const [visited, setVisited] = useState(false);
@@ -8,13 +8,19 @@ const Country = ({country}) => {
     const handleVisited = () =>{
         setVisited(!visited);
     }
+
+    // const passWithPrams = () => handleVisitedCountry(country);
+        
+    
+
     return (
-        <div className='country'>
-            <h3>Name: {name?.common}</h3>
+        <div className={`country ${visited ? 'visited': 'non-visited'}`}>
+            <h3 style={{color: visited ? 'Purple': 'white'}}>Name: {name?.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>Area: {area}</p>
             <p><small>Code: {cca3}</small></p>
+            <button onClick={() => handleVisitedCountry(country)}>Mark Visited</button> <br/>
             <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
             {visited ? 'I have visited this country.': 'I want to visited'}
         </div>
